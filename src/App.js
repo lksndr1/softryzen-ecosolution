d2a3a4efad06788e8c052715561afd2c1636a7fc
hettfield git <olekpl90@gmail.com>
2024-07-03 10:49:33 +0200

    before styling grid. without photo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles/general.css';
 import Header from './components/header/Header';
 import Mainsec from './components/main/Mainsec';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Mainsec />
+      <About />
       
     </div>
   );
